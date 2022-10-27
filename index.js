5d8b47d939c248992711e660df4b686f847d13d2
Majharul-Munna <majharulmunna21@gmail.com>
2022-10-27 19:25:50 +0600

    resolved

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000
 
 app.use(cors());
 
-const topics = require('./data/topics.json')
+const topics = require('./data/topics.json');
+// const details = require('./data/details.json');
 
 app.get('/', (req, res) => {
     res.send('Course API running');
@@ -13,8 +14,16 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
     res.send(topics)
+});
+
+app.get('/details/:id', (req, res) =>{
+    // console.log(details);
+    const  id=req.params.id;
+    const createData=topics.find(n=> n.id==id)
+    res.send(createData)
 })
 
+
 app.listen(port, () => {
-    console.log('learn with ex running on port', port);
+    console.log('ex surver running on port', port);
 })
